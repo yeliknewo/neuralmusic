d439a4af0636de344adcfb21bdb6e7ddca42380a
Kiley Owen <owenk@wit.edu>
2015-09-02 16:48:58 -0400

    added modularity

--- NeuralMusic/src/com/kileyowen/neuralmusic/Synthesizer.java
+++ NeuralMusic/src/com/kileyowen/neuralmusic/Synthesizer.java
@@ -8,32 +8,58 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class Synthesizer {
+	private AudioFormat af;
+	private SourceDataLine sdl;
+	private ByteBuffer bb;
+	private final int AUDIO_RANGE;
 
 	public Synthesizer() {
-		AudioFormat af = new AudioFormat(44100, 16, 2, true, false);
+		af = new AudioFormat(44100, 16, 2, true, false);
+		AUDIO_RANGE = (int) (Math.pow(2, af.getSampleSizeInBits()) - 1);
 		try {
-			SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
+			sdl = AudioSystem.getSourceDataLine(af);
 			sdl.open();
 			sdl.start();
-			ByteBuffer bb = ByteBuffer.allocate(4);
-			for (int i = 0; i < af.getFrameRate(); i++) {
-				
-				float frequency = 1;
-				short testSound = (short)((Math.sin(i * frequency / af.getFrameRate() * Math.PI * 2)) * Short.MAX_VALUE / 2 + Short.MAX_VALUE / 2);
-				System.out.println(testSound + " " + Short.MAX_VALUE);
-				bb.putShort(0, (short)(testSound)); // left
-				bb.putShort(2, (short)(testSound)); // right
-				sdl.write(bb.array(), 0, 4);
-			}
-			sdl.drain();
-			sdl.close();
+			bb = ByteBuffer.allocate(4);
 		} catch (LineUnavailableException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) {
-		new Synthesizer();
+	public void play(double sample) {
+		sample = Math.min(Math.max(sample, -1.0), 1.0);
+		short sampleShort = (short) (sample * AUDIO_RANGE);
+		for (int byteIndex = 0; byteIndex < af.getChannels(); byteIndex++) {
+			bb.put(byteIndex * 2 + 0, (byte) (sampleShort >> 0));
+			bb.put(byteIndex * 2 + 1, (byte) (sampleShort >> 8));
+		}
+		sdl.write(bb.array(), 0, bb.capacity());
+	}
+	
+	public void play(double[] samples){
+		for(int sampleIndex = 0;sampleIndex < samples.length;sampleIndex++){
+			play(samples[sampleIndex]);
+		}
+	}
+	
+	public void playNeural(double sampleNeural){
+		play(sampleNeural * 2 - 1);
+	}
+	
+	public void playNeural(double[] samplesNeural){
+		for(int sampleIndex = 0;sampleIndex < samplesNeural.length;sampleIndex++){
+			playNeural(samplesNeural[sampleIndex]);
+		}
 	}
 
+	public void stop() {
+		sdl.drain();
+		sdl.close();
+	}
+
+	public static void main(String[] args) {
+		Synthesizer synth = new Synthesizer();
+		
+		synth.stop();
+	}
 }
